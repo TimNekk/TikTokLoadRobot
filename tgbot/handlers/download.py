4403c4b4296257d3665e8dfc2b017ead17ea2f7b
Tim <herew26@gmail.com>
2022-07-19 00:28:22 +0300

    Wrong url exception handled

--- tgbot/handlers/download.py
+++ tgbot/handlers/download.py
@@ -2,6 +2,7 @@ from io import BytesIO
 
 from aiogram import types, Dispatcher
 from loguru import logger
+from tiktok_downloader import InvalidUrl
 
 from tgbot.models.user_tg import UserTG
 from tgbot.services.blocking_io_running import run_blocking_io
@@ -10,7 +11,14 @@ from tgbot.services.downloading import download_tiktok
 
 async def download(message: types.Message, user: UserTG):
     url = message.text
-    video: BytesIO = await run_blocking_io(download_tiktok, url)
+
+    try:
+        video: BytesIO = await run_blocking_io(download_tiktok, url)
+    except InvalidUrl:
+        await user.send_message("Неверная ссылка")
+        logger.warning(f"{user}: InvalidUrl - \"{url}\"")
+        return
+
     input_file = types.InputFile(video)
     await user.send_video(input_file)
 
