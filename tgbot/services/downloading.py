4985f3a0542b0511a937703cfbaa4dd0a34c1488
Tim <herew26@gmail.com>
2022-07-26 00:04:55 +0300

    Music downloading added

--- tgbot/services/downloading.py
+++ tgbot/services/downloading.py
@@ -1,10 +1,40 @@
+from dataclasses import dataclass
 from io import BytesIO
 
-from tiktok_downloader import info_post
+from requests.exceptions import MissingSchema
+from tiktok_downloader import info_post, InvalidUrl
 
 
-def download_tiktok(url: str) -> BytesIO:
-    info = info_post(url)
-    video = info.download()
-    video.seek(0)
-    return video
+@dataclass
+class Video:
+    data: BytesIO
+
+
+@dataclass
+class Music:
+    data: BytesIO
+    title: str
+    author: str
+
+
+def _get_info(url: str):
+    try:
+        return info_post(url)
+    except MissingSchema:
+        return info_post(f"https://{url}")
+    except KeyError:
+        raise InvalidUrl()
+
+
+def download_tiktok_video(url: str) -> Video:
+    info = _get_info(url)
+    data = info.download()
+    data.seek(0)
+    return Video(data)
+
+
+def download_tiktok_music(url: str) -> Music:
+    info = _get_info(url)
+    data = info.download_music()
+    data.seek(0)
+    return Music(data, info.music_title, info.music_author)
